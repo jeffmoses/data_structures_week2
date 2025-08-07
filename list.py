56addc7e68f09014fe199a6cc33871c6465c3cfd
jeffmoses <jeffmosesotiende@gmail.comgit config --global user,name jeffmosesgit commit -m first>
2025-08-07 17:02:14 +0300

    first_commit

---
+++ list.py
@@ -0,0 +1,27 @@
+#create an empty list called my_list.
+my_list= []
+#Append the following elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+#Insert the value 15 at the second position in the list.
+my_list.insert(1, 15)
+#Print the current state of my_list.
+print(my_list)
+#Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
+#Print the current state of my_list.
+print(my_list)
+#Remove the last element from my_list.
+my_list.pop()
+#Print the current state of my_list.
+print(my_list)
+#Sort my_list in ascending order.
+my_list.sort()
+#Print the current state of my_list.
+print(my_list)
+#Find and print the index of the value 30 in my_list.
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
